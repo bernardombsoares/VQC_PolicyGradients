bf16171e1db944458a323153cf32d6fd225f1a48
bMM-S <soaresberna@gmail.com>
2024-03-18 18:26:17 +0000

    ansatz from jerbi paper

--- Work/circuit.py
+++ Work/circuit.py
@@ -1,73 +1,74 @@
 import pennylane as qml
 import numpy as np
+from torch.nn.parameter import Parameter
 
-
-def encode(n_qubits, inputs):
-    for wire in range(n_qubits):
-        qml.RX(inputs[wire], wires=wire)
-
-
-def jerbi_layer(n_qubits, y_weight, z_weight):
-    for wire, y_weight in enumerate(y_weight):
-        qml.RY(y_weight, wires=wire)
-    for wire, z_weight in enumerate(z_weight):
-        qml.RZ(z_weight, wires=wire)
-    for wire in range(n_qubits):
-        qml.CZ(wires=[wire, (wire + 1) % n_qubits])
-
-
-
+def create_zz_operator(n_qubits):
+    ZZ = qml.PauliZ(0)
+    for i in range(1, n_qubits):
+        ZZ = qml.operation.Tensor(ZZ, qml.PauliZ(i))
+    return ZZ
 
 class CircuitGenerator:
-    def __init__(self, n_qubits, n_layers=None, shots=None):
+
+    def __init__(self, n_qubits, n_layers, shots):
         self.n_qubits = n_qubits
         self.n_layers = n_layers
         self.shots = shots
-        self.dev = qml.device("default.qubit.torch", wires=n_qubits, shots=shots)
 
 
-    def jerbi(self):
-        @qml.qnode(self.dev)
-        def qnode(self,init_params,params):
-            for layer_idx in range(self.n_layers):
-                if (layer_idx == 0):
-                    encode(self.n_qubits, inputs)
-            layer(n_qubits, y_weights[layer_idx], z_weights[layer_idx])
-        return measure(n_qubits)
+    def jerbi(self, init_params, params, measure_type = 'probs', observables = None):
 
-    model = qml.qnn.TorchLayer(circuit, shapes)
+        dev = qml.device('default.qubit', wires=self.n_qubits, shots=None, analytic=False)
+        weight_shapes = {
+                            "init_params": (self.n_layers, 2, self.n_qubits),
+                            "params": (self.n_layers + 1, 2, self.n_qubits)
+                        }
 
-    return model
+        init_method = {
+                            "init_params": torch.nn.init.normal_,
+                            "params": torch.nn.init.uniform,
+                        }
+
+        '''
+        measure_type should be either 'probs' (probability of each bitstring) or 'expval' (expectation value )
+        params should have shape=(n_layers + 1, 2, n_qubits)
+        init_params should have shape=(n_layers, 2, n_qubits)
+        '''
+
+        if measure_type != 'probs' or measure_type != 'expval':
+            return 'Error: provided measure_type is not valid.'
+        
+        qml.broadcast(qml.Hadamard, wires=range(self.n_qubits), pattern="single")
+        for i in range(self.n_layers):
+            qml.broadcast(qml.RZ, wires=range(self.n_qubits), pattern="single", parameters=params[i][0])
+            qml.broadcast(qml.RY, wires=range(self.n_qubits), pattern="single", parameters=params[i][1])
+
+            qml.broadcast(qml.CNOT, wires=range(self.n_qubits), pattern="chain")
+
+            qml.broadcast(qml.RY, wires=range(self.n_qubits), pattern="single", parameters=init_params[i][0])
+            qml.broadcast(qml.RZ, wires=range(self.n_qubits), pattern="single", parameters=init_params[i][1])
+
+        qml.broadcast(qml.RZ, wires=range(self.n_qubits), pattern="single", parameters=params[self.n_layers][0])
+        qml.broadcast(qml.RY, wires=range(self.n_qubits), pattern="single", parameters=params[self.n_layers][1])
+        
+        if measure_type == 'probs':
+            if observables == None:
+                qcircuit = qml.probs(wires=self.n_qubits)
+            else:
+                qcircuit = qml.probs(op=observables)
+
+        elif measure_type == 'expval':
+            if observables == None:
+                observables = create_zz_operator(self.n_qubits)
+                qcircuit = qml.expval(op=observables)
+            else:
+                qcircuit = qml.expval(op=observables)
+
+        circuit = qml.QNode(qcircuit, dev, interface="torch", diff_method="backprop")
+        model = qml.qnn.TorchLayer(circuit, weight_shapes=weight_shapes, init_method=init_method)
+
+        return model
 
 
-    def simplified_two_design(self, init_params, params, measurement_qubits=0, prod_approx=False):
 
-        qml.SimplifiedTwoDesign(initial_layer_weights=init_params, weights=params, wires=range(self.n_qubits))
     
-
-
-
-
-    def special_unitary(self, params):
-        return 'a'
-    
-
-
-
-
-
-    def create_circuit(self, circ="simplified_two_design", fim=False):
-        if circ == "rzry":
-            qcircuit = self.RZRY
-        elif circ == "simplified_two_design":
-            qcircuit = self.simplified_two_design
-        elif circ == "special_unitary":
-            qcircuit = self.special_unitary
-        # Define other circuit architectures similarly...
-
-        if not fim:
-            circuit = qml.QNode(qcircuit, self.dev, interface="torch", diff_method="backprop")
-        else:
-            circuit = qml.QNode(qcircuit, self.dev)
-
-        return circuit
