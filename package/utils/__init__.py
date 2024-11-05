c27eceddd7676f99f5467380104c2089bf263c69
bMM-S <soaresberna@gmail.com>
2024-11-05 20:43:59 +0000

    deleted

--- package/utils/__init__.py
+++
@@ -1,1 +0,0 @@
-from .utils import create_directory, tensor_to_list, create_optimizer_with_lr, get_function_representation,get_instance_variables
--- package/vqc/__init__.py
+++
@@ -1,2 +0,0 @@
-from .vqc_observables import measure_probs, two_measure_expval, three_measure_expval
-from .vqc_designs import JerbiModel, TfqTutorial, UQC
